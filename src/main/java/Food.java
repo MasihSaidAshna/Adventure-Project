f04dfdca7d7815c4092e4f9f3e60c53806955407
Masih <ashnacg@gmail.com>
2022-10-06 13:18:54 +0200

    For some reason github didn't add my Food class... had to recreate it in class.

---
+++ src/main/java/Food.java
@@ -0,0 +1,34 @@
+public class Food extends Item {
+
+    private int healthPoints = 100;
+
+    private int foodHealthPoints;
+
+
+    public Food(String itemName, int foodHealthPoints) {
+        super(itemName);
+        this.foodHealthPoints = foodHealthPoints;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public void setHealthPoints(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+    public int getFoodHealthPoints() {
+        return foodHealthPoints;
+    }
+
+    public void setFoodHealthPoints(int foodHealthPoints) {
+        this.foodHealthPoints = foodHealthPoints;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Food source: " + getItemName();
+    }
+}
